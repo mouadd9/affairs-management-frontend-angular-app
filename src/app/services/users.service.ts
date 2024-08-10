e9cf156cb4d0c0267472ffe21009669101f9d2cb
mouadispunk <bencaidmouad9@gmail.com>
2024-08-10 18:36:07 +0100

    added an empty service that will handle the requests regarding the operations on users

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService { // this service will have methods related to users add user delete user ..........
+
+  constructor() { }
+}
