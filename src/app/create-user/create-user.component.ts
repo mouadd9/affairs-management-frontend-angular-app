eb61a4f08a85ac5f0f620bed6b4bb6ae99b4b702
mouadispunk <bencaidmouad9@gmail.com>
2024-08-10 18:35:15 +0100

    added a TS that handles the "create-user" form data using reactive forms

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
   styleUrl: './create-user.component.css'
 })
-export class CreateUserComponent {
+export class CreateUserComponent implements OnInit {
+  myForm!: FormGroup;
+  constructor(private fb: FormBuilder){ // we will inject user service here 
+
+  }
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      // each form controle has a key value pair  
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      lastname: ['', Validators.required],
+      firstname: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.myForm.valid) {
+      // here we will send this.myForm.value (json) to our user service and then expect a response
+      const formData = this.myForm.value;
+
+    }
+  }
+
+
 
 }
