c16c72d4c8b690c5d36979307f6700757a670769
mouadispunk <bencaidmouad9@gmail.com>
2024-08-16 09:14:16 +0100

    this service will make it possible for out chart to subscribe (update) to a subject that will emit signals when a user is created

--- src/app/services/user-update.service.ts
+++ src/app/services/user-update.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserUpdateService {
 
-  constructor() { }
+export class UserUpdateService {
+  private updateSourceSubject = new Subject<void>();
+
+  update$ = this.updateSourceSubject.asObservable(); // other parts of the app will subscribe to this only which limits their capacity to emit values using .next()
+
+  notify() {
+    this.updateSourceSubject.next();
+  }
+
 }
