3cf4022bac258abcfa692d77f2124961cdf2a5f3
mouadispunk <bencaidmouad9@gmail.com>
2024-09-13 22:57:12 +0100

    changed the style for the login page and added some error handling

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -11,6 +11,11 @@ import { Router } from '@angular/router';
 
 export class LoginComponent implements OnInit {
 
+  isLogingUser: Boolean = false;
+  errorMessage: string = '';
+  successMessage: string = '';
+  hidePassword = true; 
+
   myForm!: FormGroup;
   constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) { // we will inject user service here 
 
@@ -31,7 +36,7 @@ export class LoginComponent implements OnInit {
 
   onSubmit(): void {
     if (this.myForm.valid) {
-
+      this.isLogingUser = true;
       // on submit we will store the last "myForm.value" instance (with lates values typed)
       const formData = this.myForm.value;
 
@@ -51,13 +56,49 @@ export class LoginComponent implements OnInit {
         },
         error: (error) => {
           console.error('Login failed:', error);
+          this.showError('Incorrect username or password.')
+          this.isLogingUser = false;
+          
         },
         complete: () => {
           console.log('Login request completed.');
+          this.isLogingUser = false;
         }
       });
 
+    } else {
+      this.markAllFieldsAsTouched();
+      this.showError('Fill in all fields.');
     }
+  }
+  markAllFieldsAsTouched() {
+    Object.keys(this.myForm.controls).forEach(key => {
+      const control = this.myForm.get(key);
+      control?.markAsTouched();
+    });
+  }
+
+  
+
+  togglePasswordVisibility(event: MouseEvent) {
+    event.preventDefault();  // Prevent the form from submitting
+    this.hidePassword = !this.hidePassword;
+  }
+
+  // these are used to show success/error messages 
+
+  showError(message: string) {
+    this.successMessage = '';
+    this.errorMessage = message;
+    
+    setTimeout(() => this.errorMessage = '', 7000);
+  }
+
+  showSuccess(message: string) {
+    this.errorMessage = '';
+    this.successMessage = message;
+    
+    setTimeout(() => this.successMessage = '', 7000);
   }
 
 
